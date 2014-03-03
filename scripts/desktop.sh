c68fe1d2bc37c3317183a0a43def3bfdbb9ae4af
Justin Azoff <justin.azoff@gmail.com>
2014-03-03 17:55:22 -0500

    install desktop

---
+++ scripts/desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install lubuntu-desktop --no-install-recommends
+
+USERNAME=bro
+LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
+
+# Configure lightdm autologin.
+
+if [ -f $LIGHTDM_CONFIG ]; then
+    echo "" >> $LIGHTDM_CONFIG
+    echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
+    echo "autologin-user-timeout=0" >> $LIGHTDM_CONFIG
+fi
