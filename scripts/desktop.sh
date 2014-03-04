b20705936a7aed7a4ea6f025516d24cd5f7138cb
Justin Azoff <justin.azoff@gmail.com>
2014-03-04 14:11:18 -0500

    add a terminal icon to the desktop

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -12,3 +12,7 @@ if [ -f $LIGHTDM_CONFIG ]; then
     echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
     echo "autologin-user-timeout=0" >> $LIGHTDM_CONFIG
 fi
+
+echo Add a terminal icon to the desktop
+install -o $USERNAME -g $USERNAME -d /home/${USERNAME}/Desktop/
+install -o $USERNAME -g $USERNAME /usr/share/applications/lxterminal.desktop /home/${USERNAME}/Desktop/
