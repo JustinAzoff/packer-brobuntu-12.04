cc1cd8736ee298e6b596d098bd7b3afdabc682da
Justin Azoff <justin.azoff@gmail.com>
2014-03-04 09:50:43 -0500

    fix syntax errror

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -5,7 +5,7 @@ echo "Upgrading"
 apt-get -y dist-upgrade
 
 if dpkg -l | grep linux-image-3 | grep -qv $(uname -r); then
-    echo "removing current (now obsolete) kernel
+    echo "removing current (now obsolete) kernel"
     cur=$(uname -r)
     just_version=${cur/-generic/}
     apt-get -y remove --purge linux-.*${just_version}.*
