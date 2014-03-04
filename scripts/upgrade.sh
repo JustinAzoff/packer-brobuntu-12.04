d6bb414c38a23b61574239bab430e507a9867a87
Justin Azoff <justin.azoff@gmail.com>
2014-03-04 09:11:40 -0500

    fix upgrading

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,4 +2,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Upgrading"
-apt-get -y upgrade
+apt-get -y dist-upgrade
+
+if dpkg -l | grep linux-image-3 | grep -qv $(uname -r); then
+    echo "removing current (now obsolete) kernel
+    cur=$(uname -r)
+    just_version=${cur/-generic/}
+    apt-get -y remove --purge linux-.*${just_version}.*
+fi
