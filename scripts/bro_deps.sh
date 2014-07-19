f2df47defd72734498ad2b9639b401482ee90141
Justin Azoff <justin.azoff@gmail.com>
2014-07-19 11:43:20 -0400

    ensure zsync is installed
    zsync is used for filesets

--- scripts/bro_deps.sh
+++ scripts/bro_deps.sh
@@ -2,6 +2,6 @@
 
 echo "installing bro dependencies"
 apt-get update
-apt-get -y install build-essential git
+apt-get -y install build-essential git zsync
 apt-get -y install bison flex cmake swig
 apt-get -y install libssl-dev libgeoip-dev libmagic-dev libpcap-dev python-dev libcurl4-openssl-dev
