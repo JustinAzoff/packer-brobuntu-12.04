3bfaaa84727b227e8b58a53bd7b85597a2f9be39
Justin Azoff <justin.azoff@gmail.com>
2014-07-21 14:05:59 -0400

    fix kernel
    The linux-virtual kernel apparently is not bootable.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+echo "Upgrading"
+apt-get update
+apt-get -y dist-upgrade
+
 #installing virtual kernel
 echo "removing all kernels"
 apt-get -y remove --purge linux-image.*
 
-echo "installing linux-virtual kernel"
-apt-get -y install linux-virtual
-
-echo "Upgrading"
-apt-get -y dist-upgrade
+echo "installing kernel"
+apt-get -y install linux-image
